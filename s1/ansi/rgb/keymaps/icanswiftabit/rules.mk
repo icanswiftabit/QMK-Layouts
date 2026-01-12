1267f87aacd39c8af8a19bcf80d99915226e6b19
Błażej Wdowikowski <blazej@wdowikowski.pl>
2026-01-12 11:59:44 +0100

    add s1

---
+++ s1/ansi/rgb/keymaps/icanswiftabit/rules.mk
@@ -0,0 +1,12 @@
+VIA_ENABLE = no
+
+ENCODER_MAP_ENABLE = no
+DYNAMIC_MACRO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
+CONSOLE_ENABLE = yes
+
+RGB_MATRIX_ENABLE = yes
+
+VPATH += keyboards/keychron/common
+SRC += keychron_common.c
